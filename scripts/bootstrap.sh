39385dc7476214bf35570f82981c3d84a929a4e3
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-04-15 16:34:40 -0700

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,3 +1,48 @@
 #!/usr/bin/env bash
+################################################################################
+#
+# bootstrap
+# ----------------
+# Bootstrap a container for Homelab Pull
+#
+# @author Nicholas Wilde, 0x08b7d7a3
+# @date 14 Apr 2025
+# @version 0.1.0
+#
+################################################################################
 
-# WIP
+# set -e
+# set -o pipefail
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+red=$(tput setaf 1)
+blue=$(tput setaf 4)
+default=$(tput setaf 9)
+white=$(tput setaf 7)
+
+readonly bold
+readonly normal
+readonly red
+readonly blue
+readonly default
+readonly white
+
+function print_text(){
+  echo "${blue}==> ${white}${bold}${1}${normal}"
+}
+
+function raise_error(){
+  printf "${red}%s\n" "${1}"
+  exit 1
+}
+
+# Check if variable is set
+# Returns false if empty
+function is_set(){
+  [ -n "${1}" ]
+}
+
+function command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
